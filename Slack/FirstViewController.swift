4ee5bbadec1a4e0db2a29dd72cbeca8b3a2feeef
Shunsuke Sogaya <shunsuke@Shunsuke-no-MacBook-Air-2.local>
2017-09-07 12:11:29 +0900

    Initial Commit

---
+++ Slack/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Slack
+//
+//  Created by Shunsuke Sogaya on 2017/09/07.
+//  Copyright © 2017年 Shunsuke Sogaya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
